355c0f617814c348fe918aad7e2382c1bbb1ac8c
Ergash0 <92915363+Ergash0@users.noreply.github.com>
2021-11-16 20:38:03 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+<body background="https://lh3.googleusercontent.com/proxy/Aff3HxC5lHD3xFrHvqD7i_1GbGjGj_iUg8dijOoNfI7p0HisY0GHASjAI7LIQdSmtFGqOQUlA2sysWY">
+
+<style>
+   .parent {
+   padding: 10px;
+}
+img {
+  position: relative;
+  top: 100%; 
+  left: 100%;
+  height: 50%;
+  width: 50%;
+}
+   .round {
+    border-radius: 10000px;
+    border: px solid green;
+    box-shadow: 0 0 7px #666;
+   </style>
+<style>
+   rightpic {
+   float: right; /* Выравнивание по правому краю */
+   margin: 0 0 5px 5px; /* Отступы вокруг фотографии */
+}</style>
+
+<h1 style="font-size: 50px">PORTFOLIO</h1>
+<hr>
+<h2 style="font-size: 50px">Personal data</h2>
+
+<p><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfoGeLAvchsleg7l9smkL4VkvNS3Pnezms0g&usqp=CAU"  width="100" height="100" align="right" class="round"></p>
+
+<p>Name:_____Rustambek</p>
+<hr>
+<p>Surname:____Zhanabilov</p>
+<hr>
+<p>Birthday:___09.03.2004</p>
+<hr>
+<p>Class:____Csc-20a</p>
+<hr>
+<p>Phone:____+99670433439</p>
+<hr>
+<p>Adress:_____Sokuluk,_Novopavlovka</p>
+<hr>
+<div class="container">
+    <form class="modal-content animate" action="https://www.instagram.com/">
+    <div class="imgcontainer">
+    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+    <button type="submit">Instagram</button>
+</label>
+</div>
+<hr>
+<div class="container">
+    <form class="modal-content animate" action="https://www.facebook.com/">
+    <div class="imgcontainer">
+    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+    <button type="submit">Facebook</button>
+</label>
+</div>
+<hr>
+<div class="container">
+    <form class="modal-content animate" action="https://www.youtube.com/">
+    <div class="imgcontainer">
+    <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">×</span>
+    <button type="submit">YouTube</button>
+</label>
+</div>
+<hr>
+</body>
+</html>
